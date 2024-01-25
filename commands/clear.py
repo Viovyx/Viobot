b066638460d67da45ab04de8125acafddf1a22a9
Renzo Rouma <renzo.rouma@gmail.com>
2024-01-25 22:36:16 +0100

    change `/clear` command usage

--- commands/clear.py
+++ commands/clear.py
@@ -8,14 +8,14 @@ class Clear(Extension):
     @slash_command(
         name="clear",
         dm_permission=False,
-        sub_cmd_name="amount",
-        sub_cmd_description="Clear a certain amount of messages",
+        sub_cmd_name="messages",
+        sub_cmd_description="Clear a certain amount of messages. Default is 50 from everyone.",
         options=[
             interactions.SlashCommandOption(
                 name="amount",
                 description="How many messages should be deleted?",
                 type=interactions.OptionType.INTEGER,
-                required=True
+                required=False
             ),
             interactions.SlashCommandOption(
                 name="user",
@@ -25,24 +25,7 @@ class Clear(Extension):
             ),
         ],
     )
-    async def clear(self, ctx: SlashContext, amount: int, user: interactions.Member=None):
+    async def clear(self, ctx: SlashContext, amount: int = 50, user: interactions.Member = None):
         log_command(ctx=ctx, cmd="clear.amount")
         deleted = await ctx.channel.purge(deletion_limit=amount, predicate=lambda m: m.author.id == user.id if user else True)
-        await ctx.send(f"{deleted} messages deleted {'from ' + user.mention if user else ''}", ephemeral=True)
-
-    @clear.subcommand(
-        sub_cmd_name="all",
-        sub_cmd_description="Clear all messages in the current channel",
-        options=[
-            interactions.SlashCommandOption(
-                name="user",
-                description="From which user should the messages be deleted?",
-                type=interactions.OptionType.USER,
-                required=False
-            ),
-        ],
-    )
-    async def clear_all(self, ctx: SlashContext, user: interactions.Member=None):
-        log_command(ctx=ctx, cmd="clear.all")
-        deleted = await ctx.channel.purge(predicate=lambda m: m.author.id == user.id if user else True)
         await ctx.send(f"{deleted} messages deleted {'from ' + user.mention if user else ''}", ephemeral=True)
